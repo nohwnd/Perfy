6a0ca8a0071b014d64f766c0d5e21545522ce2e8
Jakub Jareš <me@jakubjares.com>
2022-04-08 14:18:20 +0200

    Fix name in uri

--- Perfy.TestAdapter/Program.cs
+++ Perfy.TestAdapter/Program.cs
@@ -14,7 +14,7 @@ public class VsTestExecutor : ITestDiscoverer, ITestExecutor
     static VsTestExecutor() {
         Count = int.TryParse(Environment.GetEnvironmentVariable("TEST_COUNT") ?? "1000", out var count) ? count : 1000;
         }
-    public const string Id = "executor://intent.testadapter";
+    public const string Id = "executor://perfy.testadapter";
     public static readonly Uri Uri = new Uri(Id);
 
     public static int Count { get; }
