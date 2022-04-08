f0f4a0a7f4c5c8e4a5f265d478f6e5b0484743c0
Jakub Jareš <me@jakubjares.com>
2022-04-08 13:54:01 +0200

    Perfy adapter that has almost no overhead

---
+++ Perfy.TestAdapter/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
+using System.Data;
+
+Console.WriteLine("Hello, World!");
+
+[ExtensionUri(Id)]
+[DefaultExecutorUri(Id)]
+public class VsTestExecutor : ITestDiscoverer, ITestExecutor
+{
+    static VsTestExecutor() {
+        Count = int.TryParse(Environment.GetEnvironmentVariable("TEST_COUNT") ?? "1000", out var count) ? count : 1000;
+        }
+    public const string Id = "executor://intent.testadapter";
+    public static readonly Uri Uri = new Uri(Id);
+
+    public static int Count { get; }
+
+    public void Cancel()
+    {
+        // noop
+    }
+
+    public void RunTests(IEnumerable<TestCase> tests, IRunContext runContext, IFrameworkHandle frameworkHandle)
+    {
+        var sources = tests.Select(t => t.Source).Distinct().ToList();
+        RunTests(sources, runContext, frameworkHandle);
+    }
+
+    public void RunTests(IEnumerable<string> sources, IRunContext runContext, IFrameworkHandle frameworkHandle)
+    {
+        var location = typeof(VsTestExecutor).Assembly.Location;
+        for (var i = 0; i < Count; i++) {
+            var tc = new TestCase($"Test{i}", Uri, location);
+            frameworkHandle.RecordResult(new TestResult(tc)
+            {
+                Outcome = TestOutcome.Passed
+            });
+        }
+    }
+
+    public void DiscoverTests(IEnumerable<string> sources, IDiscoveryContext discoveryContext, IMessageLogger logger, ITestCaseDiscoverySink discoverySink)
+    {
+        var location = typeof(VsTestExecutor).Assembly.Location;
+        for (var i = 0; i < Count; i++)
+        {
+            discoverySink.SendTestCase(new TestCase($"Test{i}", Uri, location));
+        }   
+    }
+}
---
+++ Run.ps1
@@ -0,0 +1,43 @@
+﻿param (
+    [ValidateSet("Debug", "Release")]
+    [string] $Configuration = "Debug"
+)
+
+dotnet build --configuration $Configuration
+
+$path = "$PSScriptRoot\Perfy.TestAdapter\bin\$Configuration\net7.0\adapter\Perfy.TestAdapter.dll"
+
+$results = [System.Collections.Generic.List[Object]]@()
+
+$steps = @("Discovery", "Run")
+$counts = @(1, 100, 1000, 10000)
+foreach ($step in $steps) {
+    foreach ($count in $counts) {
+        $env:TEST_COUNT = $count
+        $sw = [Diagnostics.StopWatch]::StartNew()
+
+        if ($step -eq "Discovery") {
+            # --list-tests does discovery only, the output is long and adds writing to console to overhead
+            # we dont care about that, so we capture it and ignore it.
+            $null = dotnet test $path --list-tests
+            "Discovering $count tests took $($sw.ElapsedMilliseconds) ms"
+        }
+        else {
+            # run output is short, it is nice to see it
+            dotnet test $path
+        }
+
+        $results.Add([pscustomobject]@{
+            Step = $Step
+            Count = $count
+            Time = "$($sw.ElapsedMilliseconds) ms"
+            ExitCode = $LASTEXITCODE
+        })
+    } 
+}
+
+$results
+
+if ($results.ExitCode -gt 0) { 
+    throw "At least one step failed."
+}
